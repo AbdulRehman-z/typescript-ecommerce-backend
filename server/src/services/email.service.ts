4e85572145f7e934fd41a9f3672933686ad42a35
Abdul Rehman <yousafbhaikhan10@gmail.com>
2023-07-10 20:50:54 +0500

    modified email service

--- server/src/services/email.service.ts
+++ server/src/services/email.service.ts
@@ -2,7 +2,7 @@ import * as dotenv from "dotenv";
 dotenv.config();
 import nodemailer from "nodemailer";
 import { ProductDoc } from "../models/Product";
-import { OrderDoc } from "../models/Order";
+import { OrderDoc, OrderStatus } from "../models/Order";
 import { Address } from "../models/User";
 
 // Send reset email
@@ -34,8 +34,6 @@ export const sendResetEmail = async (email: string, resetToken: string) => {
   }
 };
 
-// Send order confirmation email
-
 export const sendOrderConfirmationEmail = async (
   email: string,
   address: Address,
@@ -52,15 +50,13 @@ export const sendOrderConfirmationEmail = async (
       },
     });
 
-    const mailOptions = {
-      from: process.env.ADMIN_EMAIL_SRV,
-      to: email,
-      subject: "Order Confirmation",
-      text: `Thank you for your order!`,
-      html: `<!DOCTYPE html>
-<html>
-<head>
-  <style>
+    let emailSubject = "";
+    let emailText = "";
+    let emailHtml = "";
+
+    const styles = `
+
+    <style>
     body {
       font-family: Arial, sans-serif;
     }
@@ -75,8 +71,8 @@ export const sendOrderConfirmationEmail = async (
     }
 
     img {
-      width: 40px !important;
-      height: 40px !important;
+      width: 40px;
+      height: 40px;
     }
 
     th, td {
@@ -88,51 +84,257 @@ export const sendOrderConfirmationEmail = async (
     th {
       background-color: #f2f2f2;
     }
-  </style>
-</head>
-<body>
-  <h1>Thank you for your order!</h1>
-  <h2>Order Details</h2>
-  <p>Order ID: ${order._id}</p>
-  <p>Order Status: ${order.status}</p>
-  
-  <h2>Shipping Address</h2>
-  <p>Address: ${address.street}</p>
-  <p>Country: ${address.country}</p>
-  <p>State: ${address.state}</p>
-  <p>Zip Code: ${address.zipCode}</p>
-  <p>Phone Number: ${address.phoneNumber}</p>
-  
-  <h2>Order Items</h2>
-  <table>
-    <tr>
-      <th>Product</th>
-      <th>Quantity</th>
-      <th>Price</th>
-    </tr>
-    ${products.map((product: any) => {
-      return `
-      <tr>
-        <td>${product.productToUpdate.title}</td>
-        <td>${product.quantity}</td>
-        <td>${product.quantity} X ${product.productToUpdate.price} = ${
-        product.quantity * product.productToUpdate.price
-      }</td>
-      </tr>`;
-    })}
-  </table>
-  
-  <h2>Order Summary</h2>
-  <p>Total: ${order.totalPrice}</p>
-  
-  <h2>Thank you for shopping with us!</h2>
-</body>
-</html>`,
+
+    </style>
+    `;
+
+    // Set email content based on the order status
+    switch (order.status) {
+      case OrderStatus.Pending:
+        emailSubject = "Order Confirmation - Pending";
+        emailText =
+          "Thank you for your order! It is currently being processed.";
+        emailHtml = `<!DOCTYPE html>
+          <html>
+          <head>
+            <style>
+              body {
+                font-family: Arial, sans-serif;
+              }
+            
+              h1, h2 {
+                color: #333;
+              }
+            
+              table {
+                border-collapse: collapse;
+                width: 100%;
+              }
+            
+              img {
+                width: 40px;
+                height: 40px;
+              }
+            
+              th, td {
+                border: 1px solid #ddd;
+                padding: 8px;
+                text-align: left;
+              }
+            
+              th {
+                background-color: #f2f2f2;
+              }
+            </style>
+          </head>
+          <body>
+            <h1>Thank you for your order!</h1>
+            <h2>Order Details</h2>
+            <p>Order ID: ${order._id}</p>
+            <p>Order Status: ${order.status}</p>
+            
+            <h2>Shipping Address</h2>
+            <p>Address: ${address.street}, ${address.state}, ${
+          address.zipCode
+        }, ${address.country}</p>
+            
+            <h2>Ordered Products</h2>
+            <table>
+              <tr>
+                <th>Product</th>
+                <th>Quantity</th>
+                <th>Price</th>
+              </tr>
+              ${products.map((product: any) => {
+                return `
+                <tr>
+                  <td>${product.productToUpdate.title}</td>
+                  <td>${product.quantity}</td>
+                  <td>${product.quantity} X ${
+                  product.productToUpdate.price
+                } = ${product.quantity * product.productToUpdate.price}</td>
+                </tr>`;
+              })}
+            </table>
+            
+            <h2>Order Summary</h2>
+            <p>Total: ${order.totalPrice}</p>
+            
+            <h2>Thank you for shopping with us!</h2>
+          </body>
+          </html>`;
+        break;
+      case OrderStatus.Processing:
+        // Add CSS styling for processing status
+        emailSubject = "Order Confirmation - Processing";
+        emailText =
+          "Thank you for your order! It is currently being processed.";
+        emailHtml = `<!DOCTYPE html>
+          <html>
+          <head>
+            <style>
+             ${styles}
+            </style>
+          </head>
+          <body>
+            <h1>Thank you for your order!</h1>
+            <h2>Shipping Address</h2>
+              <p>Address: ${address.street}, ${address.state}, ${address.zipCode}, ${address.country}</p>
+            <h2>Order Details</h2>
+             <p>Order Status: ${order.status}</p>
+            <h2>Thank you for shopping with us!</h2>
+          </body>
+          </html>`;
+        break;
+      case OrderStatus.Shipped:
+        // Add CSS styling for shipped status
+        emailSubject = "Order Confirmation - Shipped";
+        emailText =
+          "Thank you for your order! It has been shipped and is on its way to you.";
+        emailHtml = `<!DOCTYPE html>
+          <html>
+          <head>
+            <style>
+            ${styles}
+            </style>
+          </head>
+          <body>
+            <h1>Thank you for your order!</h1>
+            <h2>Shipping Address</h2>
+              <p>Address: ${address.street}, ${address.state}, ${address.zipCode}, ${address.country}</p>
+            <h2>Order Details</h2>
+             <p>Order Status: ${order.status}</p>
+            <h2>Thank you for shopping with us!</h2>
+          </body>
+          </html>`;
+        break;
+      case OrderStatus.Delivered:
+        // Add CSS styling for delivered status
+        emailSubject = "Order Confirmation - Delivered";
+        emailText = "Thank you for your order! It has been delivered to you.";
+        emailHtml = `<!DOCTYPE html>
+          <html>
+          <head>
+            <style>
+            ${styles}
+            </style>
+          </head>
+          <body>
+            <h1>Thank you for your order!</h1>
+            <h2>Shipping Address</h2>
+              <p>Address: ${address.street}, ${address.state}, ${address.zipCode}, ${address.country}</p>
+            <h2>Order Details</h2>
+              <p>Order Status: ${order.status}</p>
+            <h2>Thank you for shopping with us!</h2>
+          </body>
+          </html>`;
+        break;
+      case OrderStatus.Cancelled:
+        // Add CSS styling for cancelled status
+        emailSubject = "Order Confirmation - Cancelled";
+        emailText = "Thank you for your order! It has been cancelled.";
+        emailHtml = `<!DOCTYPE html>
+          <html>
+          <head>
+            <style>
+            ${styles}
+            </style>
+          </head>
+          <body>
+            <h1>Your order has been cancelled!</h1>
+            <h2>Order Details</h2>
+              <p>Order Status: ${order.status}</p>
+            <h2>Please contact us if you have any questions.</h2>
+            <h2>Here is our contact information:</h2>
+             <p>Phone: 123-456-7890</p>
+             <p>Email: ${process.env.ADMIN_EMAIL_SRV}</p>
+            <h2>Thank you for shopping with us!</h2>
+          </body>
+          </html>`;
+        break;
+      case OrderStatus.Returned:
+        // Add CSS styling for returned status
+        emailSubject = "Order Confirmation - Returned";
+        emailText = "Your order has been returned.";
+        emailHtml = `<!DOCTYPE html>
+          <html>
+          <head>
+            <style>
+            ${styles}
+            </style>
+          </head>
+          <body>
+            <h1>Your order has been returned!</h1>
+            <h2>Order Details</h2>
+              <p>Order Status: ${order.status}</p>
+            <h2>Please contact us if you have any questions.</h2>
+            <h2>Here is our contact information:</h2>
+              <p>Phone: 123-456-7890</p>
+              <p>Email: ${process.env.ADMIN_EMAIL_SRV}</p>
+            <h2>We will be happy to help you!</h2>
+          </body>
+          </html>`;
+        break;
+
+      case OrderStatus.Refunded:
+        // Add CSS styling for refunded status
+        emailSubject = "Order Confirmation - Refunded";
+        emailText = "Your order has been refunded.";
+        emailHtml = `<!DOCTYPE html>
+          <html>
+          <head>
+           <style>
+           ${styles}
+           </style>
+          </head>
+          <body>
+            <h1>Your order has been refunded!</h1>
+            <h2>Order Details</h2>
+              <p>Order Status: ${order.status}</p>
+            <h2>Please contact us if you have any questions.</h2>
+            <h2>A refund will be issued to your original payment method.</h2>
+            <h2>Here is our contact information:</h2>
+              <p>Phone: 123-456-7890</p>
+              <p>Email: ${process.env.ADMIN_EMAIL_SRV}</p>
+            <h2>We will be happy to help you!</h2>
+          </body>
+          </html>`;
+        break;
+
+      default:
+        // Add default CSS styling for other order statuses
+        emailSubject = "Order Confirmation";
+        emailText = "Thank you for your order!";
+        emailHtml = `<!DOCTYPE html>
+          <html>
+          <head>
+            <style>
+            ${styles}
+            </style>
+          </head>
+          <body>
+            <h1>Thank you for your order!</h1>
+            <h2>Shipping Address</h2>
+              <p>Address: ${address.street}, ${address.state}, ${address.zipCode}, ${address.country}</p>
+            <h2>Order Details</h2>
+              <p>Order Status: ${order.status}</p>
+            <h2>Thank you for shopping with us!</h2>
+          </body>
+          </html>`;
+        break;
+    }
+
+    const mailOptions = {
+      from: process.env.ADMIN_EMAIL_SRV,
+      to: email,
+      subject: emailSubject,
+      text: emailText,
+      html: emailHtml,
     };
 
     // Send the email
     await transporter.sendMail(mailOptions);
   } catch (error) {
-    throw new Error("Error sending order confirmation email" + error);
+    throw new Error("Error sending order confirmation email: " + error);
   }
 };
